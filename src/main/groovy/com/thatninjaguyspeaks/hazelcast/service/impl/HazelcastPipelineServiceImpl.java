2e89793e84f1b258a71e67ae29d4535185d1ed8e
DeadshotTech <125792873+DeadshotTech@users.noreply.github.com>
2023-11-09 20:58:45 +0530

    (checkpoint)

--- src/main/groovy/com/thatninjaguyspeaks/hazelcast/service/impl/HazelcastPipelineServiceImpl.java
+++ src/main/groovy/com/thatninjaguyspeaks/hazelcast/service/impl/HazelcastPipelineServiceImpl.java
@@ -6,6 +6,7 @@ import com.hazelcast.jet.pipeline.Sinks;
 
 import com.hazelcast.jet.pipeline.SourceBuilder;
 import com.hazelcast.jet.pipeline.SourceBuilder.SourceBuffer;
+import com.hazelcast.jet.pipeline.test.TestSources;
 import com.hazelcast.map.IMap;
 import com.thatninjaguyspeaks.hazelcast.config.HazelcastClientInitializer;
 import com.thatninjaguyspeaks.hazelcast.service.HazelcastPipelineService;
@@ -16,6 +17,9 @@ import org.springframework.stereotype.Service;
 
 
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 @Service
 public class HazelcastPipelineServiceImpl implements HazelcastPipelineService {
@@ -29,23 +33,35 @@ public class HazelcastPipelineServiceImpl implements HazelcastPipelineService {
         var hz = hazelcastClientInitializer.getHazelcastInstance();
         var p = Pipeline.create();
 
-        IMap<Long, Integer> map = hz.getMap("myMap");
+        IMap<Long, Long> map = hz.getMap("myMap");
         logger.info("Map Size: {}", map.size());
 
-        JobConfig jobConfig = new JobConfig();
-        jobConfig.addClass(RandomEntriesSupplier.class);
+//        JobConfig jobConfig = new JobConfig();
+//        jobConfig.addClass(RandomEntriesSupplier.class);
+//        jobConfig.addClass(RandomEntriesSupplier.FillBufferRunnable.class);
 
-        p.readFrom(SourceBuilder.batch("random-numbers-source", RandomEntriesSupplier::new)
-                        .fillBufferFn(RandomEntriesSupplier::fillBuffer)
-                        .build())
-                .writeTo(Sinks.mapWithUpdating("myMap",
-                        Map.Entry::getKey,
-                        (oldValue, entry) -> entry.getValue()));
+        p.readFrom(TestSources.itemStream(10))
+                .withoutTimestamps()
+                .filter(event -> event.sequence() % 2 == 0)
+                .setName("filter out odd numbers")
+                .writeTo(Sinks.logger());
+//        p.readFrom(SourceBuilder.batch("random-numbers-source", ctx -> new RandomEntriesSupplier(10_000_000).applyEx(ctx))
+//                        .fillBufferFn(RandomEntriesSupplier.FillBufferRunnable::fillBuffer)
+//                        .build())
+//                .writeTo(Sinks.mapWithUpdating("myMap",
+//                        Map.Entry::getKey,
+//                        (oldValue, entry) -> entry.getValue()));
 
-        hz.getJet().newJob(p, jobConfig).join();
+//        p.readFrom(SourceBuilder.batch("random-numbers-source", ctx -> new RandomEntriesSupplier(10_000_000))
+//                        .fillBufferFn((RandomEntriesSupplier.FillBufferRunnable supplier, SourceBuffer<Map.Entry<Long, Long>> buffer)
+//                                -> supplier.fillBuffer(buffer))
+//                        .build())
+//                .writeTo(Sinks.mapWithUpdating("myMap",
+//                        Map.Entry::getKey,
+//                        (oldValue, entry) -> entry.getValue()));
 
-        hz.getJet().newJob(p, jobConfig).join();
-
+//        hz.getJet().newJob(p, jobConfig);
+        hz.getJet().newJob(p);
         logger.info("Map Size: {}", map.size());
         hz.shutdown();
         logger.info("Triggered pipeline");
--- src/main/groovy/com/thatninjaguyspeaks/hazelcast/service/impl/RandomEntriesSupplier.java
+++ src/main/groovy/com/thatninjaguyspeaks/hazelcast/service/impl/RandomEntriesSupplier.java
@@ -1,27 +1,53 @@
 package com.thatninjaguyspeaks.hazelcast.service.impl;
 
-import com.hazelcast.jet.core.Processor;
-import com.hazelcast.jet.pipeline.SourceBuilder;
-
-import java.util.AbstractMap;
+import java.io.Serializable;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class RandomEntriesSupplier {
-    private final long limit;
-    private long count = 0;
+import com.hazelcast.function.FunctionEx;
+import com.hazelcast.jet.core.Processor;
+import com.hazelcast.jet.pipeline.SourceBuilder.SourceBuffer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-    public RandomEntriesSupplier(Processor.Context context) {
-        limit = 10_00_00_000;
+public class RandomEntriesSupplier implements FunctionEx<Processor.Context, RandomEntriesSupplier.FillBufferRunnable>, Serializable {
+
+    private static final Logger logger = LogManager.getLogger(RandomEntriesSupplier.class);
+    private final long limit;
+
+    public RandomEntriesSupplier(long limit) {
+        this.limit = limit;
     }
 
-    public void fillBuffer(SourceBuilder.SourceBuffer<Map.Entry<Long, Integer>> buffer) {
-        while (count < limit) {
-            buffer.add(new AbstractMap.SimpleEntry<>(count, ThreadLocalRandom.current().nextInt()));
-            count++;
-            if (count == limit) {
-                buffer.close();
-            }
+    @Override
+    public FillBufferRunnable applyEx(Processor.Context context) throws Exception {
+        return new FillBufferRunnable(limit);
+    }
+
+    public static class FillBufferRunnable implements Serializable {
+        private static final Logger logger = LogManager.getLogger(FillBufferRunnable.class);
+        private final long limit;
+        private transient long count = 0;
+
+        public FillBufferRunnable(long limit) {
+            this.limit = limit;
+        }
+
+        public void fillBuffer(SourceBuffer<Map.Entry<Long, Long>> buffer) {
+            logger.info("fillBuffer called with buffer: {} ",buffer);
+//            while (count < limit) {
+//                var value = ThreadLocalRandom.current().nextLong();
+                var value = count;
+                logger.info("fillBuffer executing with key:{}, value: {}", count, value);
+                buffer.add(new SimpleEntry<>(count, value));
+                count++;
+                if (count == limit) {
+                    buffer.close();
+                }
+//            }
+            logger.info("fillBuffer completed with buffer: {}", buffer!=null?buffer:"EMPTY");
         }
     }
 }
+
